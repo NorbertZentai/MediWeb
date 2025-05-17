f3e3ffe5021aa844818521a8859c35809ccfc827
Norbert Zentai <zentai.norbert96@gmail.com>
2025-05-17 04:18:47 +0200

    added login feature

---
+++ MediTrack_Frontend/src/components/RequireAuth.js
@@ -0,0 +1,13 @@
+import { useLocation, Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+export default function RequireAuth({ children }) {
+  const { user } = useAuth();
+  const location = useLocation();
+
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
+}
