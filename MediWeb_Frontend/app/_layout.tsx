8a47536a01e5f7665710b630e71c77572f1ec52f
Norbert Zentai <zentai.norbert96@gmail.com>
2025-08-11 16:14:18 +0200

    Fix frontend issues: disable problematic custom fonts temporarily

--- MediWeb_Frontend/app/_layout.tsx
+++ MediWeb_Frontend/app/_layout.tsx
@@ -13,19 +13,25 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+  // Disable custom fonts for now to fix font loading issues
+  // const [loaded] = useFonts({
+  //   SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  // });
 
+  // useEffect(() => {
+  //   if (loaded) {
+  //     SplashScreen.hideAsync();
+  //   }
+  // }, [loaded]);
+
+  // if (!loaded) {
+  //   return null;
+  // }
+
+  // Hide splash screen immediately since we're not loading custom fonts
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
+    SplashScreen.hideAsync();
+  }, []);
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
