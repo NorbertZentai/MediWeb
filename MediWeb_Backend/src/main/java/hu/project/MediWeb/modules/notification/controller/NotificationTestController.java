ea426887f044200231ca1d52a144536b506e97a3
Norbert Zentai <zentai.norbert96@gmail.com>
2025-08-11 15:40:37 +0200

    Temporarily disable notification services to fix JavaMailSender dependency

--- MediWeb_Backend/src/main/java/hu/project/MediWeb/modules/notification/controller/NotificationTestController.java
+++ MediWeb_Backend/src/main/java/hu/project/MediWeb/modules/notification/controller/NotificationTestController.java
@@ -5,7 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
 @RequiredArgsConstructor
 public class NotificationTestController {
 
