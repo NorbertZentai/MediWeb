5d3d953cb4f912ebf8f2f03cc2fe82191977b902
Norbert Zentai <zentai.norbert96@gmail.com>
2025-10-23 12:59:31 +0200

    refactor(frontend): remove web push notifications #15

--- MediWeb_Frontend/src/App.js
+++ MediWeb_Frontend/src/App.js
@@ -1,24 +1,14 @@
 import { AuthProvider } from 'contexts/AuthContext';
 import AppRouter from 'routes/AppRouter';
 import { ToastProvider } from 'components/ToastProvider';
-import { usePushNotifications } from 'features/notification/usePushNotifications';
 import "styles/App.css";
-
-// Component that uses the push notifications hook inside AuthProvider
-function AppWithNotifications() {
-  usePushNotifications();
-  
-  return (
-    <ToastProvider>
-      <AppRouter />
-    </ToastProvider>
-  );
-}
 
 export default function App() {
   return (
     <AuthProvider>
-      <AppWithNotifications />
+      <ToastProvider>
+        <AppRouter />
+      </ToastProvider>
     </AuthProvider>
   );
 }
